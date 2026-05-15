c08636fe79a5dfd6c5e5399e4b2aec138ef01ae7
sainath1923 <vsainath.ui@gmail.com>
2026-05-15 18:44:33 +0400

    Logo size increased

--- procurement/src/App.jsx
+++ procurement/src/App.jsx
@@ -111,7 +111,7 @@ function Login({ onLoggedIn }) {
               <img
                 src={companyLogo}
                 alt="Company logo"
-                style={{ height: '36px', objectFit: 'contain' }}
+                style={{ height: '75px', objectFit: 'contain' }}
               />
             )}
             <div>
@@ -652,7 +652,7 @@ function ProcurementDashboard({ onLogout }) {
               <img
                 src={companyLogo}
                 alt="Company logo"
-                style={{ height: '36px', objectFit: 'contain' }}
+                style={{ height: '75px', objectFit: 'contain' }}
               />
             )}
             <div>
