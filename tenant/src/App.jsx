83139606a8bcf03fc747f8997b25ecb554855777
sainath1923 <vsainath.ui@gmail.com>
2026-05-15 18:45:11 +0400

    Logo size increased

--- tenant/src/App.jsx
+++ tenant/src/App.jsx
@@ -111,7 +111,7 @@ function Login({ onLoggedIn }) {
               <img
                 src={companyLogo}
                 alt="Company logo"
-                style={{ height: '36px', objectFit: 'contain' }}
+                style={{ height: '75px', objectFit: 'contain' }}
               />
             )}
             <div>
@@ -277,7 +277,7 @@ function TenantDashboard({ onLogout }) {
               <img
                 src={companyLogo}
                 alt="Company logo"
-                style={{ height: '36px', objectFit: 'contain' }}
+                style={{ height: '75px', objectFit: 'contain' }}
               />
             )}
             <div>
